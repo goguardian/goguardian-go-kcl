09bcce10f1b3037d19c7ab3954c67f1447d97b5d
Sohil Gogri <sohil@goguardian.com>
2021-04-08 15:51:15 -0700

    Need to test

--- sample/processor.go
+++ sample/processor.go
@@ -7,31 +7,34 @@ import (
 )
 
 type sampleProcessor struct {
-	logger           *log.Logger
-	latestCheckpoint string
+	logger               *log.Logger
+	latestSequenceNumber string
 }
 
 func (s *sampleProcessor) Initialize(input *kcl.InitializationInput) {
 	s.printInput("Initialize", input)
 }
 
-func (s *sampleProcessor) ProcessRecords(input *kcl.ProcessRecordsInput) {
+func (s *sampleProcessor) ProcessRecords(input *kcl.ProcessRecordsInput) error {
 	s.printInput("ProcessRecords", input)
 	for _, record := range input.Records {
-		s.latestCheckpoint = record.SequenceNumber
+		s.latestSequenceNumber = record.SequenceNumber
 	}
+	return input.Checkpoint(s.latestSequenceNumber)
 }
 
 func (s *sampleProcessor) LeaseLost(input *kcl.LeaseLostInput) {
 	s.printInput("LeastLost", input)
 }
 
-func (s *sampleProcessor) ShardEnded(input *kcl.ShardEndedInput) {
+func (s *sampleProcessor) ShardEnded(input *kcl.ShardEndedInput) error {
 	s.printInput("ShardEnded", input)
+	return input.Checkpoint(s.latestSequenceNumber)
 }
 
-func (s *sampleProcessor) ShutdownRequested(input *kcl.ShutdownRequestedInput) {
+func (s *sampleProcessor) ShutdownRequested(input *kcl.ShutdownRequestedInput) error {
 	s.printInput("ShutdownRequested", input)
+	return input.Checkpoint(input.SequenceNumber)
 }
 
 func (s *sampleProcessor) printInput(inputType string, input interface{}) {
