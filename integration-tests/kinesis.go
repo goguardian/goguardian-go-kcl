37a79ba393dd605454e9dd0ff47878597f96d9a0
kevin <kevin@goguardian.com>
2021-05-14 14:35:58 -0700

    Move waitForLocalstack function into another file

--- integration-tests/kinesis.go
+++ integration-tests/kinesis.go
@@ -1,10 +1,6 @@
 package integration_tests
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"net/http"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -154,40 +150,4 @@ func (t *localKinesis) PutRecords(streamName string, records []string) error {
 	}
 
 	return nil
-}
-
-func waitForLocalstack(timeout time.Duration) error {
-	ticker := time.NewTicker(timeout)
-	defer ticker.Stop()
-
-	for {
-		select {
-		case <-ticker.C:
-			return errors.New("timed out waiting for localstack to start")
-		default:
-		}
-
-		resp, err := http.Get(fmt.Sprintf("%s/health", localstackEndpoint))
-		if err != nil {
-			continue
-		}
-		defer resp.Body.Close()
-
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			return errors.Wrap(err, "failed to read localstack health")
-		}
-
-		health := struct {
-			Services map[string]string `json:"services"`
-		}{}
-		err = json.Unmarshal(body, &health)
-		if err != nil {
-			return errors.Wrap(err, "failed to parse localstack health")
-		}
-
-		if status, found := health.Services["kinesis"]; found && status == "running" {
-			return nil
-		}
-	}
 }
